f384276df4538740a21b005f8089542872ddfa9f
Aarush <aarushsingh1305@gmail.com>
2026-08-23 12:29:23 +0530

    Fixed bugs

--- main.go
+++ main.go
@@ -1,39 +1,74 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"log"
 	"net"
+	"os"
+	"strings"
 )
 
 func main() {
 	listener, err := net.Listen("tcp", ":8080")
 	if err != nil {
-		log.Printf("Internal Server Error: %v\n", err)
+		log.Fatalf("Internal Server Error: %v\n", err)
 	}
 
-	listServer, err := net.Listen("tcp", ":9090")
+	connServer, err := net.Dial("tcp", "localhost:9090")
 	if err != nil {
-		log.Printf("Internal Server Error: %v\n", err)
+		log.Fatalf("Internal Server Error: %v\n", err)
 	}
 
-	connServer, err := listServer.Accept()
-	if err != nil {
-		log.Printf("Error connecting to server: %v\n", err)
-	}
-	go handleServer(connServer)
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
 			log.Printf("Error connecting to server: %v\n", err)
+			continue
 		}
-		go handleConnection(conn)
+		go handleConnection(connServer, conn)
 	}
 }
 
-func handleConnection(c net.Conn) {
+func handleConnection(cServer net.Conn, c net.Conn) {
+	defer c.Close()
 
+	reader := bufio.NewReader(c)
+	stdout := bufio.NewWriter(os.Stdout)
+	writer := bufio.NewWriter(cServer)
+	host, _, _ := net.SplitHostPort(c.RemoteAddr().String())
+
+	for lineNumber := 1; ; lineNumber++ {
+		if lineNumber == 2 {
+			fmt.Fprintf(writer, "X-Forwarded-For: %s\r\n", host)
+			writer.Flush()
+		}
+		line, _ := reader.ReadString('\n')
+		stdout.WriteString(line)
+		stdout.Flush()
+		handleServer(line, cServer)
+
+		if line == "\r\n" || line == "\n" {
+			break
+		}
+	}
 }
 
-func handleServer(c net.Conn) {
+func handleServer(line string, c net.Conn) {
+	writer := bufio.NewWriter(c)
+	stdout := bufio.NewWriter(os.Stdout)
 
+	parts := strings.Split(line, ":")
+
+	switch parts[0] {
+	case "Host":
+		line = fmt.Sprintf("Host: %s\r\n", c.RemoteAddr().String())
+	case "Connection":
+		line = ""
+	}
+
+	writer.WriteString(line)
+	writer.Flush()
+	stdout.WriteString(line)
+	stdout.Flush()
 }
